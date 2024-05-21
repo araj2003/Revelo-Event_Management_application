16d316f121e2384872f08fe0bf5cdbae307acc97
araj <araj.jain2003@gmail.com>
2024-05-21 07:32:49 +0530

    created group and message contraller

--- server/controllers/chats.ts
+++ server/controllers/chats.ts
@@ -47,40 +47,79 @@ const createSingleChat = async (req: Request, res: Response) => {
   }
 };
 
-//create group(add users from subevent id)
-// const createGrupChat = async(req: Request, res: Response) => {
-//     const {groupName,}
-//     if (!req.body.users || !req.body.name) {
-//         return res.status(400).send({
-//             message: "please fill the fields"
-//         });
-//     }
+// create group(add users from subevent id)
+const createGrupChat = async(req: Request, res: Response) => {
+    const {groupName,allUsers,channelId} = req.body
+    if (!groupName || !allUsers) {
+        return res.status(400).send({
+            message: "please fill the fields"
+        });
+    }
 
-//     var users = JSON.parse(req.body.users);
-//     if (users.length < 2) {
-//         return res.status(400).json("more than 2 users to form a group chat");
-//     }
+    var users = JSON.parse(allUsers);
+    if (users.length < 2) {
+        return res.status(400).json("more than 2 users to form a group chat");
+    }
 
-//     // Extract user IDs and add them to the array
-//     users.push(req.user.id);
+    // Extract user IDs and add them to the array
+    users.push(req.user.userId);
 
-//     try {
-//         const groupChat = await Chat.create({
-//             chatName: req.body.name,
-//             users: users,
-//             isGroupChat: true,
-//             groupAdmin: req.user.id
-//         });
+    try {
+        const groupChat = await Chat.create({
+            chatName: req.body.name,
+            channelId:channelId,
+            users: users,
+            isGroupChat: true,
+            groupAdmin: req.user.userId
+        });
 
-//         const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
-//             .populate("users", "-password")
-//             .populate("groupAdmin", "-password");
+        const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password");
 
-//         res.status(200).json(fullGroupChat);
-//     } catch (error) {
-//         res.status(500).json({
-//             msg: "cannot create group chat",
-//             error: error.message
-//         });
-//     }
+        res.status(200).json({
+          groups:fullGroupChat,
+          msg:"group chat created"
+        });
+    } catch (error) {
+        res.status(500).json({
+            msg: "cannot create group chat",
+            error: error
+        });
+    }
+}
+
+//get single chat
+// const getSingleChat = async(req: Request, res: Response) => {
+
 // }
+
+
+
+//get all chat
+const getAllChats = async(req: Request, res: Response) => {
+  try {
+    await Chat.find({users:{$elemMatch:{$eq:req.user.userId}}})
+    .populate("users","-password")
+    .populate("groupAdmin","-password")
+    .populate("latestMessage")
+    .sort({updatedAt:-1})
+    .then(async(results:any)=> {
+        results = await User.populate(results,{
+            path:"latestMessage.sender",
+            select:"name email profilePicture role"
+        })
+
+        res.status(200).send(results)
+    })
+} catch (error) {
+    res.json({
+        msg:"error getting chats",
+        error:error
+    })
+  }
+}
+
+export = {
+  createGrupChat,createSingleChat,getAllChats
+}
---
+++ server/routes/message.ts
@@ -0,0 +1,24 @@
+// import express from "express";
+// import authMiddleWare from "../middleware/authentication";
+
+// const router = express.Router();
+
+// import {
+//     sendMessage,
+//     getMessage
+// } from "../controllers/message";
+
+// //middleware (only host can do it)
+
+// //send message ..
+// router.post("/sendMessage",authMiddleWare ,sendMessage);
+
+// //get message ..
+// router.get("/getMessage",authMiddleWare , getMessage);
+
+
+
+
+
+
+// export default router;
