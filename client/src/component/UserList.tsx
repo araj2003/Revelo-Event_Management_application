2eaeba7d2926127dff9b71bfd59c02e2dadf1c27
Carbrex <95964955+Carbrex@users.noreply.github.com>
2024-05-26 23:37:22 +0530

    userlist

---
+++ client/src/component/UserList.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import profImg from "@/../assets/logo-xero@2x.png";
+
+const defaultUsers = [
+  {
+    name: "Neil Sims",
+    email: "email@email@windster.com",
+  },
+  {
+    name: "Bonnie Green",
+    email: "email@bonnie.com",
+  },
+  {
+    name: "Michael Gough",
+    email: "email@michael.com",
+  },
+  {
+    name: "Lana Byrd",
+    email: "email@lana.com",
+  },
+];
+
+function UserList() {
+  return (
+    <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex items-center justify-between mb-4">
+        <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+          Event Members
+        </h5>
+        {/* <a
+          href="#"
+          className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+        >
+          View all
+        </a> */}
+      </div>
+      <div className="flow-root">
+        <ul
+          role="list"
+          className="divide-y divide-gray-200 dark:divide-gray-700"
+        >
+          {defaultUsers.map((user) => (
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <img
+                    className="w-8 h-8 rounded-full"
+                    src={profImg}
+                    alt="Neil image"
+                  />
+                </div>
+                <div className="flex-1 min-w-0 ms-4">
+                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                    {user.name}
+                  </p>
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                    {user.email}
+                  </p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default UserList;
--- client/src/pages/Chat/Chat.tsx
+++ client/src/pages/Chat/Chat.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import Message from "../../component/Message/Message";
 import ChatInput from "../../component/ChatInput/ChatInput";
 import Editor from "../../component/Editor/Editor";
+import UserList from "@/component/UserList";
 
 const Chat = () => {
   const { roomId } = useParams<{ roomId: string }>();
@@ -31,33 +32,36 @@ const Chat = () => {
     },
   ]);
   return (
-    <div className="chat">
-      <div className="chat__header">
-        <div className="chat__headerLeft">
-          <h4 className="chat_channelName">
-            <strong>#General</strong>
-            <StarBorderIcon />
-          </h4>
+    <>
+      <div className="chat">
+        <div className="chat__header">
+          <div className="chat__headerLeft">
+            <h4 className="chat_channelName">
+              <strong>#General</strong>
+              <StarBorderIcon />
+            </h4>
+          </div>
+          <div className="chat__headerRight">
+            <p>
+              <InfoIcon /> Details
+            </p>
+          </div>
         </div>
-        <div className="chat__headerRight">
-          <p>
-            <InfoIcon /> Details
-          </p>
+        <div className="chat__messages">
+          {channelMessages.map(({ message, time, user, userImage }) => (
+            <Message
+              message={message}
+              time={time}
+              user={user}
+              userImage={userImage}
+            />
+          ))}
         </div>
+        <ChatInput />
+        {/* <Editor /> */}
       </div>
-      <div className="chat__messages">
-        {channelMessages.map(({ message, time, user, userImage }) => (
-          <Message
-            message={message}
-            time={time}
-            user={user}
-            userImage={userImage}
-          />
-        ))}
-      </div>
-      <ChatInput />
-      {/* <Editor /> */}
-    </div>
+      <UserList />
+    </>
   );
 };
 
