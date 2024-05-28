b9d746e4fbc3636a12cecb2baf5cc568e5e3701d
Carbrex <95964955+Carbrex@users.noreply.github.com>
2024-05-28 17:40:03 +0530

    Refactor Calender component and update event handling

--- client/src/component/calender/Calender.tsx
+++ client/src/component/calender/Calender.tsx
@@ -1,50 +1,50 @@
-import React,{useContext, useEffect,useState} from 'react'
-import {Calendar,momentLocalizer} from 'react-big-calendar'
-import moment from 'moment'
+import React, { useContext, useEffect, useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 import "./calender.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { getSubEvents } from '@/api';
-import { EventContext } from '@/context/EventContext';
+import { getSubEvents } from "@/api";
+import { EventContext } from "@/context/EventContext";
 // import { date } from 'zod';
 const Calender = () => {
-    const {eventId} = useContext(EventContext)
-    const [data,setData] = useState([])
-    const [myEventList,setMyEventList] = useState<any>([])
-    useEffect(() => {
-      const getEvent = async() => {
-        const response :any= await getSubEvents(eventId)
-        console.log(response?.subEvents)
-        setData(response?.subEvents)
-        response?.subEvents?.forEach((event:any) => {
-          setMyEventList((prev:any) => [...prev,{
-            title:event.subEventName,
-            start:event.subEventDate,
-            end:event.subEventTime,
-          }])
-        })
-      }
+  const { eventId } = useContext(EventContext);
+  const [data, setData] = useState([]);
+  const [myEventList, setMyEventList] = useState<any>([]);
+  useEffect(() => {
+    const getEvent = async () => {
+      const response: any = await getSubEvents(eventId);
+      console.log(response?.subEvents);
+      setData(response?.subEvents);
+      response?.subEvents?.forEach((event: any) => {
+        setMyEventList((prev: any) => [
+          ...prev,
+          {
+            title: event.subEventName,
+            start: new Date(event.subEventDate) || new Date(),
+            end: new Date(event.subEventTime) || new Date(),
+          },
+        ]);
+      });
+    };
 
-      getEvent()
-      
-      
-    },[eventId])
-    console.log("eventList",myEventList)
-    const localizer = momentLocalizer(moment)
+    getEvent();
+  }, [eventId]);
+  console.log("eventList", myEventList);
+  const localizer = momentLocalizer(moment);
 
-    
   return (
     <div className="calendar">
-    <div className="calendar__container">
-    <Calendar
-      localizer={localizer}
-      events={myEventList}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 500 }}
-    />
-  </div>
-  </div>
-  )
-}
+      <div className="calendar__container">
+        <Calendar
+          localizer={localizer}
+          events={myEventList}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Calender
+export default Calender;
