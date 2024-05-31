ed22149af483c862d2d725ce20d5e309768ebb48
Mohd Sahil <ms9929997@gmail.com>
2024-05-31 15:52:59 +0530

    fix the dropdown hover

--- client/src/component/Header/Header.tsx
+++ client/src/component/Header/Header.tsx
@@ -83,11 +83,11 @@ const Header = () => {
         {/* header right */}
       </div>
 
-      <div className="relative">
+      <div className="relative mr-3">
         <button
           id="dropdownDelayButton"
           onClick={() => setShowDropdown(!showDropdown)}
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-purple-600 dark:hover:bg-purple-800 dark:focus:ring-purple-800"
           type="button"
         >
           Dropdown hover{" "}
