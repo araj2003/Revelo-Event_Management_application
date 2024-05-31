b530bbd2e54c1dc1f9c9b4ebfea77fd77d3bda4b
araj <araj.jain2003@gmail.com>
2024-05-31 23:16:40 +0530

    hello

--- server/models/User.ts
+++ server/models/User.ts
@@ -43,6 +43,7 @@ const UserSchema = new mongoose.Schema<IUser>(
       enum: ["vendor", "regular"],
       default: "regular",
     },
+    
     subroll: {
       type: String,
       required: function () {
