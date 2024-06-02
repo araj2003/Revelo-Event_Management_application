e4f1ca636a93431164d51c69002ff6ee0ba494dd
Carbrex <95964955+Carbrex@users.noreply.github.com>
2024-06-03 00:50:49 +0530

    chore: Update EventCard and Chat components

--- client/src/component/MyEvent/EventCard.tsx
+++ client/src/component/MyEvent/EventCard.tsx
@@ -6,9 +6,9 @@ const EventCard = ({event}:any) => {
     <div className="event-list  ">
       <a
         href="#"
-        className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-slate-50 dark:border-gray-700"
       >
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900  ">
           {event?.serverName}
         </h5>
         <p className="font-normal text-gray-700 dark:text-gray-400">
@@ -18,7 +18,7 @@ const EventCard = ({event}:any) => {
           id="dropdownUsersButton"
           data-dropdown-toggle="dropdownUsers"
           data-dropdown-placement="bottom"
-          className="text-white bg-slate-700 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+          className="text-white bg-purple-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
           type="button"
           onClick={() => setShowMembers(!showMembers)}
         >
@@ -41,10 +41,10 @@ const EventCard = ({event}:any) => {
         </button>
         <div
           id="dropdownUsers"
-          className={`z-10 ${showMembers ? "absolute" : "hidden"} bg-white rounded-lg shadow w-60 dark:bg-gray-600`}
+          className={`z-10 ${showMembers ? "absolute" : "hidden"} bg-slate-100 rounded-lg shadow w-60`}
         >
           <ul
-            className="h-48 py-2 overflow-y-auto text-gray-700 dark:text-gray-200"
+            className="h-48 py-2 overflow-y-auto text-gray-700"
             aria-labelledby="dropdownUsersButton"
           >
             {event?.users.map((user: any) => {
@@ -52,7 +52,7 @@ const EventCard = ({event}:any) => {
                 <li>
                   <a
                     href="#"
-                    className="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    className="flex items-center px-4 py-2 hover:bg-gray-100 "
                   >
                     <img
                       className="w-6 h-6 me-2 rounded-full"
--- client/src/component/MyEvent/MyEvent.tsx
+++ client/src/component/MyEvent/MyEvent.tsx
@@ -74,7 +74,7 @@ const MyEvent = () => {
         </Link>
       </div>
       <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center">
-        <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 ">
+        <span className="text-purple-600">
           My Events
         </span>{" "}
       </h1>
